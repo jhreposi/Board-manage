ae5903b32555faea05f1b57bcd297492ee3054df
edit-jh <jhsygo@gmail.com>
2024-09-10 15:28:54 +0900

    refactor: Moved ArticleInfoDto to inner class in ArticleResponseDto

--- src/test/java/com/example/board/BoardAdminApplicationTests.java
+++ src/test/java/com/example/board/BoardAdminApplicationTests.java
@@ -1,11 +1,7 @@
 package com.example.board;
 
-import com.example.board.dto.ArticleInfoDto;
-import com.example.board.model.Article;
-import com.example.board.model.Category;
 import com.example.board.repository.ArticleMapper;
 import org.junit.jupiter.api.Test;
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -15,48 +11,6 @@ class BoardAdminApplicationTests {
     void contextLoads() {
     }
 
-    @Test
-    public void toDto() {
-
-        Category category = Category.builder()
-                .categoryId(2)
-                .name("공지")
-                .build();
-
-        Article article = Article.builder()
-                .articleId(1)
-                .title("제목")
-                .createdAt("2024")
-                .content("내용")
-                .pinnedYn("Y")
-                .secretYn("N")
-                .category(category)
-                .build();
-
-
-
-
-
-        ModelMapper modelMapper = new ModelMapper();
-
-//        modelMapper.addMappings(new PropertyMap<ArticleInfo, ArticleInfoDto>() {
-//            @Override
-//            protected void configure() {
-//                map(source.getArticle().getArticleId(), destination.getArticleId());
-//
-//            }
-//        });
-
-//        TypeMap<ArticleInfo, ArticleInfoDto> typeMap = modelMapper.createTypeMap(ArticleInfo.class, ArticleInfoDto.class);
-//        typeMap.addMappings(mapper -> mapper.map(src -> src.getBoard().getBoardId(), ArticleInfoDto::setBoardId ));
-
-        ArticleInfoDto articleInfoDto = modelMapper.map(article, ArticleInfoDto.class);
-
-        System.out.println(articleInfoDto.toString());
-
-
-
-    }
     @Test
     public void mapping(ArticleMapper mapper) {
         mapper.selectNoticeList().stream().forEach(article -> {
