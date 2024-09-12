1395d960b67605068481e30e7553999fef33a49c
edit-jh <jhsygo@gmail.com>
2024-09-12 13:02:14 +0900

    fix: 일부페이지 거치고 로그인시 세션 생성 안됨
    8d960765258587016c8f5f24aa3aea6ccd44614
    
    작업에서 공용컴포넌트 생성에서 HttpSession을 주입받기떄문에 로그인 전에 이미 세션이 생성이 되어서 isNew()값이 false로 인해 생성이 안되었었음 getSession(false)로 변경하여 빈값의 세션 생성 방지로 해결

--- src/main/java/com/example/board/controller/ArticleController.java
+++ src/main/java/com/example/board/controller/ArticleController.java
@@ -5,6 +5,7 @@ import com.example.board.dto.ArticleReqDto;
 import com.example.board.dto.ArticleResDto;
 import com.example.board.model.Article;
 import com.example.board.service.ArticleService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,11 +28,26 @@ public class ArticleController {
     ArticleService articleService;
 
     @ModelAttribute("adminInfo")
-    public AdminRes.InfoDto adminInfo(HttpSession session) {
-        if (session.getAttribute("adminInfo") != null) {
-            return (AdminRes.InfoDto)session.getAttribute("adminInfo");
+    public AdminRes.InfoDto adminInfo(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        AdminRes.InfoDto adminInfo = null;
+        // TODO: 2024-09-12 로그인페이지 말고 모든 페이지의 레이아웃이 있는데 매번 페이지 호출할떄마다 세션값 확인하는것이 합리적인가?
+        // 아니면 처음 로그인하면 세션스토리지에 저장해서 하는것 고려
+        /**
+         * 여기서 httpsession을 생성을 해버려서
+         * 로그인 할시 session.isNew()가 계속 false였음
+         * 아티클 페이지 안타고 바로 로그인주소로 가서 로그인을 할떄만 isNew() true여서
+         * 아티클 페이지 에서 로그인하면 session생성을 못해 레이아웃에 정보표시를 못했다
+         * 1. 그전에 주입을 HttpSession으로 받아서 그런가? => X
+         * HttpServletRequest주입받고
+         * HttpSession session = request.getSession(); 로변경했지만 여기 거치고 로그인하면 isNew() false
+         * 2. HttpSession session = request.getSession(false);
+         * 로 변경해야 기존세션없으니 빈세션 생성을 안함
+         */
+        if (session != null) {
+            adminInfo = (AdminRes.InfoDto)session.getAttribute("adminInfo");
         }
-        return null;
+        return adminInfo;
     }
 
     @GetMapping("notice")
